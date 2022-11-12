0c0b57747a8bc09b5887eea40bed6ae0e5030ede
Kel-VinTech <mondaykelvintech@gmail.com>
2022-11-11 19:23:51 -0500

    created a project

---
+++ Random password/index.js
@@ -0,0 +1,25 @@
+// let PassId = document.querySelector('password');
+
+let passGenerator = document.querySelector('.generate').addEventListener('click',generatePass)
+
+function generatePass(e) {
+    var chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*"; 
+    var passLength = 12;
+    var pass = "";
+
+    for (var i = 0; i <= passLength; i++) {
+        var randomPass = Math.floor(Math.random() * chars.length)
+        pass += chars.substring(randomPass, randomPass+1);
+    }
+    document.getElementById('password').value = pass;
+    console.log(pass);
+}
+ 
+
+let copyPasswod = document.querySelector('.copy').addEventListener('click',copyPass);
+function copyPass(e) {
+    copyText = document.getElementById('password');
+    copyText.select();
+    copyText.setSelectionRange(0,999)
+    document.execCommand("copy")
+}
