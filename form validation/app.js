717441023833efa210355c3f88764755c275e527
Kel-VinTech <mondaykelvintech@gmail.com>
2022-11-14 20:38:11 -0500

    updated project

--- form validation/app.js
+++ form validation/app.js
@@ -1,20 +1,19 @@
 
-let formValidation = ()  => {
+function formValidation (e) {
+   
     // name valid
-    // var name = document.querySelector('.name');
-    // var fName = document.querySelector('.Fname');
-    // if(name.value == "") {
-    //     name.nextElementSibling.style.display = "block";
-    //     fName.style.border = "1px solid #f00";
-    //     return false;
+    var name = document.querySelector('.name');
+    var fName = document.querySelector('.Fname');
+    if(name.value == "") {
+        fName.style.border = "1px solid #f00";
+        return false;
     
-    // }else {
-    //     fName.style.border = "1px solid transparent";
-    // }
-    // console.log(name.value)
+    }else {
+        fName.style.border = "1px solid transparent";
+    }
 
+    // // // for email
 
-    // // for email
     var email = document.querySelector('.email');
     var femail = document.querySelector('.femail');
     var mailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -24,31 +23,67 @@ let formValidation = ()  => {
         femail.style.border = "1px solid transparent";
     }
 
-    // for phone number
+    // // // for phone number
+
     var phone = document.querySelector('.phone');
     var fphone = document.querySelector('.fphone');
-    var phoneno = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    var phoneno = /^[\+]?[(]?[0-9]{2,3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
     if(!phone.value.match(phoneno) || phone.value == "") {
         fphone.style.border = "1px solid #f00";
     }else {
         fphone.style.border = "1px solid transparent";
     }
 
+    // // for password
 
-    // for password
+    var password = document.querySelector('.password');
+    var fpass = document.querySelector('.fpass');
     var decimal=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    if(!password.value.match(decimal)|| password.value == "") {
+        fpass.style.border = "1px solid #f00";
+    }else {
+        fpass.style.border = "1px solid transparent";
+    }
 
+    // // for password confirmation
 
-
+    var confirm = document.querySelector('.confirm');
+    var repass = document.querySelector('.repass');
+    var conirmPass =  password.value;
+    if(!confirm.value.match(conirmPass)|| confirm.value == "") {
+        repass.style.border = "1px solid #f00";
+    }else {
+        repass.style.border = "1px solid transparent";
+    }
 
     // // checkbox
-    // var checkbox = document.querySelector('.input [type = "checkbox"]');
-    // checkbox.addEventListener('change',  () => {
-    //     if(checkbox.checked) {
-    //         alert("Please accept terms and conditions")
-    //     }
-    // })
+    var checkbox = document.querySelector('.checkbox');   
+    if(!checkbox.checked === true){
+        alert('Please accept Terms & Conditions');
+    }else{
+        return false;
+    }
     
-    // console.log("checkbox")
+}
+// to show and hide password
+let showp = document.getElementById('show').addEventListener('click',showPass);
+function showPass () {
+    var show = document.querySelector('.password');
+    if(show.type === "password") {
+        show.type = "text"; 
+    } else {
+        show.type = "password";
+    }
 }
 
+// to show and hide confirmation password
+
+let reshow = document.getElementById('reshow').addEventListener('click',reShowPass);
+function reShowPass () {
+    var show = document.querySelector('.confirm');
+    if(show.type === "password") {
+        show.type = "text"; 
+    } else {
+        show.type = "password";
+    }
+}
